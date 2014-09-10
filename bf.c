9a1111097e68abad10d44f94131aa176f15f91e4
Vierkantor <vierkantor@vierkantor.com>
2014-09-10 13:47:29 +0000

    Rename things even worse.

--- bf.c
+++ bf.c
@@ -1,44 +1,32 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-char*lI,Il[11l];*I1;
-// "global"s
-// I1[0]: I1pointer
-// I1[1]: I1size
-// I1[2]: lIpointer
-// "local"s
-// I1[3]: indent
-// I1[4]: instruction index
-
-left(){--*I1;}
-right(){++*I1;}
-plus(){++I1[*I1];}
-min(){--I1[*I1];}
-dot(){printf(Il+9, I1[*I1]);}
-comma(){I1[*I1] = getchar();}
-
-bra(){I1[*I1]||(moveToKet(I1[3]=1));}
-moveToKet(){I1[3]&&(I1[2]++,lI[I1[2]]-Il[6]||I1[3]++,lI[I1[2]]-Il[7]||I1[3]--,moveToKet());}
-
-ket(){I1[*I1]&&(moveToBra(I1[3]=1));}
-moveToBra(){I1[3]&&(I1[2]--,lI[I1[2]]-Il[7]||I1[3]++,lI[I1[2]]-Il[6]||I1[3]--,moveToBra());}
+char*A,B[11l];*C;
+D(){--*C;}
+E(){++*C;}
+F(){++C[*C];}
+G(){--C[*C];}
+H(){C[*C] = getchar();}
+I(){printf(B+9, C[*C]);}
+J(){C[*C]||(L(C[3]=1));}
+K(){C[*C]&&(M(C[3]=1));}
+L(){C[3]&&(C[2]++,A[C[2]]-B[6]||C[3]++,A[C[2]]-B[7]||C[3]--,L());}
+M(){C[3]&&(C[2]--,A[C[2]]-B[7]||C[3]++,A[C[2]]-B[6]||C[3]--,M());}
 
 // (*instr)()
-(*instrs[])()= {left, right, plus, min, comma, dot, bra, ket};
+(*N[])()= {D, E, F, G, H, I, J, K};
 
-IlReadLoop(){Il[I1[4]]&&(lI[I1[2]]-Il[I1[4]]||instrs[I1[4]](),IlReadLoop(I1[4]++));}
+O(){B[C[4]]&&(A[C[2]]-B[C[4]]||N[C[4]](),O(C[4]++));}
+
+P(){A[C[2]]&&(O(C[4] = 0),*C-C[1]||(C=realloc(C,sizeof(int)*C[1]<<1),memset(C+C[1],B[8],C[1]*sizeof(int)),C[1]<<=1),P(++(C[2])));}
+Q(){(C[0]-12)&&(B[*C] = B[C[0] - 1] + 2.0 + (-21.0 + (21.0 + (-11.0 + (4.0 + (-23.0/30.0 + (-0.1 + (139.0/1260.0 + (-1277.0/40320.0 + (31.0/8640.0 + (251.0*(-10.0 + (double)*C))/453600.0)*(-9.0 + (double)*C))*(-8.0 + (double)*C))* (-7.0 + (double)*C))*(-6.0 + (double)*C))*(-5.0 + (double)*C))*(-4.0 + (double)*C))*(-3.0 + (double)*C))*(-2.0 + (double)*C))* (-1.0 + (double)*C) + 0.001,Q((*C)++));}
+main(int argc, char**argv) {C=calloc(8,sizeof(int)),A=argv[1],Q(B[0] = ';' + (C[0] = 1)),C[0]=5,C[1]=8,P();}
 
 // delme!
-printI1(){
-	printf("mp=%d I1=%d pp=%d ins=%c (%d)\n", *I1, I1[*I1], I1[2], lI[I1[2]], lI[I1[2]]);
+printMem(){
+	printf("mp=%d C=%d pp=%d ins=%c (%d)\n", *C, C[*C], C[2], A[C[2]], A[C[2]]);
 	int i;
 	for(i = 1; i < 4; i++) {
-		printf("%d: %d (%c)\n", i, I1[i], I1[i]);
+		printf("%d: %d (%c)\n", i, C[i], C[i]);
 	}
 }
-
-doLoop(){/*printI1();*/lI[I1[2]]&&(IlReadLoop(I1[4] = 0),*I1-I1[1]||(I1=realloc(I1,sizeof(int)*I1[1]<<1),memset(I1+I1[1],Il[8],I1[1]*sizeof(int)),I1[1]<<=1),doLoop(++(I1[2])));}
-
-CharLoop(){(I1[0]-12)&&(Il[*I1] = Il[I1[0] - 1] + 2.0 + (-21.0 + (21.0 + (-11.0 + (4.0 + (-23.0/30.0 + (-0.1 + (139.0/1260.0 + (-1277.0/40320.0 + (31.0/8640.0 + (251.0*(-10.0 + (double)*I1))/453600.0)*(-9.0 + (double)*I1))*(-8.0 + (double)*I1))* (-7.0 + (double)*I1))*(-6.0 + (double)*I1))*(-5.0 + (double)*I1))*(-4.0 + (double)*I1))*(-3.0 + (double)*I1))*(-2.0 + (double)*I1))* (-1.0 + (double)*I1) + 0.001,CharLoop((*I1)++));}
-
-main(int argc, char**argv) {I1=calloc(8,sizeof(int)),lI=argv[1],CharLoop(Il[0] = ';' + (I1[0] = 1)),I1[0]=5,I1[1]=8,doLoop();}
